18591a79d011f6288f81e0d4a8bd7d358e2319ec
Faiz Muhammad <tubagusrofi0810@gmail.com>
2023-02-13 15:40:27 +0700

    Menambahkan Adapter Dan Model

---
+++ app/src/main/java/com/example/relativelayout_tubagusmuhammadrofialfaiz_xiirpl/model/CategoriesModel.kt
@@ -0,0 +1,12 @@
+package com.example.relativelayout_tubagusmuhammadrofialfaiz_xiirpl.model
+
+class CategoriesModel : java.io.Serializable {
+    @JvmField
+    var strCategory:String?=null
+
+    @JvmField
+    var strCategoryThumb:String?=null
+
+    var strCategoryDescription:String?=null
+
+}
---
+++ app/src/main/java/com/example/relativelayout_tubagusmuhammadrofialfaiz_xiirpl/networking/Api.kt
@@ -0,0 +1,8 @@
+package com.example.relativelayout_tubagusmuhammadrofialfaiz_xiirpl.networking
+
+object Api {
+    var Categories = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
+
+}
+
